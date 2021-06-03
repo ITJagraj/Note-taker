a42de6aff93763afd9f92a5e17b6d9a577953c50
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-06-03 16:43:17 -0400

    set up port

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+//Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
