a38294b768e9a2e62f48c0d55a2a4421db83a70b
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-06-26 15:12:25 -0400

    newfiles

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
+const util = require('util');
 
 //Initialize express app
 const app = express();
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static("public"));
 
 
 //set variables 
